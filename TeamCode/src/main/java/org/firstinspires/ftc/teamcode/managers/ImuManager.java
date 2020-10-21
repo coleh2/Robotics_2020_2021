5937aaaaf831bea74d43cb69a9962db1fc41c04d
coleh <coleh@coleh.net>
2020-10-21 19:52:04 -0400

    Add IMU

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/managers/ImuManager.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.managers;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+public class ImuManager {
+    public BNO055IMU imu;
+
+    public ImuManager(BNO055IMU imu) {
+        this.imu = imu;
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.useExternalCrystal = true;
+        parameters.loggingEnabled  = false;
+
+        imu.initialize(parameters);
+    }
+
+    /**
+     * Get the orientation
+     * @return Current orientation of the robot, in degrees
+     */
+    public Orientation getOrientation() {
+        return imu.getAngularOrientation(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+    }
+}
