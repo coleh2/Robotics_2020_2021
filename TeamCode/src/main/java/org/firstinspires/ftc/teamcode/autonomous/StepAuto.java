49de4519909b698be2ed65f19e8ca250eec601bb
santiago-galan <santigalan04@gmail.com>
2021-02-27 17:07:25 -0500

    StepAuto / Frankie Issues persist

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/StepAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/StepAuto.java
@@ -3,7 +3,10 @@ package org.firstinspires.ftc.teamcode.autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.managers.*;
 
 enum step {
     START, MOVE1, MOVE2, MOVE3, MOVE4, MOVE5, MOVE6, MOVE7, MOVE8, MOVE9, MOVE10, MOVE11, MOVE12, MOVE13, MOVE14, MOVE15, MOVE16, MOVE17, MOVE18, MOVE19, MOVE20, MOVE21, MOVE22, MOVE23, MOVE24, MOVE25, MOVE26, MOVE27, MOVE28, MOVE29, MOVE30, MOVE31, MOVE32;
@@ -17,6 +20,9 @@ public class StepAuto extends OpMode {
     int numberCalled = 0;
     double referPoint = 0;
 
+    MovementManager driver;
+    ManipulationManager limbs;
+
     public step getNext() {
         step[] x = step.values();
         int i = 0;
@@ -27,7 +33,10 @@ public class StepAuto extends OpMode {
     }
 
     public void init() {
-
+        driver = new MovementManager(hardwareMap.get(DcMotor.class, "fl"),
+                hardwareMap.get(DcMotor.class, "fr"),
+                hardwareMap.get(DcMotor.class, "bl"),
+                hardwareMap.get(DcMotor.class, "br"));
     }
     public void loop() {
 
@@ -48,6 +57,33 @@ public class StepAuto extends OpMode {
 
         if (timer.milliseconds() - referPoint >= milliseconds) {
             numberCalled = 0;
+            currentStep = getNext();
+        }
+    }
+
+    void nextStepStop(int milliseconds) {
+
+        if(timer == null)
+            timer = new ElapsedTime();
+        if(currentStep == null)
+            currentStep = step.START;
+
+
+
+        numberCalled++;
+        if(numberCalled == 1) {
+            referPoint = timer.milliseconds();
+        }
+
+        if (timer.milliseconds() - referPoint >= milliseconds) {
+
+            driver = new MovementManager(hardwareMap.get(DcMotor.class, "fl"),
+                    hardwareMap.get(DcMotor.class, "fr"),
+                    hardwareMap.get(DcMotor.class, "bl"),
+                    hardwareMap.get(DcMotor.class, "br"));
+
+            numberCalled = 0;
+            driver.driveRaw(0f, 0f, 0f, 0f);
             currentStep = getNext();
         }
     }
