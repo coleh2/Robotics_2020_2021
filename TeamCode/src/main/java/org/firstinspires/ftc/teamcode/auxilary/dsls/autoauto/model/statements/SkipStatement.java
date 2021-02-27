552f9e6c1b196c8827edf21402411e31995a8a11
coleh <coleh@coleh.net>
2021-02-27 15:22:59 -0500

    Annotate tostring as notnull

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auxilary/dsls/autoauto/model/statements/SkipStatement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auxilary/dsls/autoauto/model/statements/SkipStatement.java
@@ -1,11 +1,14 @@
 package org.firstinspires.ftc.teamcode.auxilary.dsls.autoauto.model.statements;
 
+import org.jetbrains.annotations.NotNull;
+
 public class SkipStatement extends Statement {
     public int delta;
     public SkipStatement() {}
     public SkipStatement(String src) {
         this.delta = Integer.parseInt(src.substring("skip ".length()));
     }
+    @NotNull
     public String toString() {
         return "skip " + delta;
     }
