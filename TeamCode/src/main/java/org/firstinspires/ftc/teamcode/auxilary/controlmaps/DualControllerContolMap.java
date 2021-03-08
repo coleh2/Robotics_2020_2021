b2b4d85bbc50292c82fadfc3d65a967ae2e56f34
superturtle5 <adamcarita@gmail.com>
2021-03-08 16:30:03 -0500

    Add joshua's best cross button

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auxilary/controlmaps/DualControllerContolMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auxilary/controlmaps/DualControllerContolMap.java
@@ -3,10 +3,10 @@ package org.firstinspires.ftc.teamcode.auxilary.controlmaps;
 public class DualControllerContolMap extends ControlMap {
     public static String drive = "ternary( or(or(leftStickY, leftStickX), rightStickX),vector3(deadzone(leftStickY, 0.1), deadzone(leftStickX, 0.05), rightStickX),vector3(scale(deadzone(gamepad2LeftStickY, 0.1), 0.4), scale(deadzone(gamepad2LeftStickX, 0.05), 0.4), scale(gamepad2RightStickX, 0.4)))";
     public static String intake = "ternary(cross, -1, ternary(triangle, 1, ternary(leftTrigger, -1, 0)))";
-    public static String drum = "ternary(dpadUp, -1, ternary(dpadDown, 1, ternary(gamepad2DpadDown, 1, ternary(leftTrigger, -1, ternary(gamepad2DpadUp, -1, 0)))))";
+    public static String drum = "ternary(dpadUp, -1, ternary(dpadDown, 1, ternary(gamepad2DpadDown, 1, ternary(leftTrigger, -1, ternary(gamepad2Cross, -1, ternary(gamepad2DpadUp, -1, 0))))))";
     public static String shooterArm = "ternary(greaterThan(gamepad2RightTrigger, 0.1), 0, 0.63)";
-    public static String flywheelRight = "scale(ternary(gamepad2B, -0.3, ternary(gamepad2LeftTrigger, 1, 0)), -1)";
-    public static String flywheelLeft  = "scale(ternary(gamepad2B, -0.3, ternary(gamepad2LeftTrigger, 1, 0)), 1)";
+    public static String flywheelRight = "scale(ternary(gamepad2Cross, -0.3, ternary(gamepad2LeftTrigger, 1, 0)), -1)";
+    public static String flywheelLeft  = "scale(ternary(gamepad2Cross, -0.3, ternary(gamepad2LeftTrigger, 1, 0)), 1)";
     public static String wobbleGrabRight = "ternary(gamepad2RightBumper, 1, 0)";
     public static String wobbleGrabLeft = "ternary(gamepad2RightBumper, 1, 0)";
     public static String wobbleArmRight = "ternary(gamepad2LeftBumper, 0.055, 0.086)";
