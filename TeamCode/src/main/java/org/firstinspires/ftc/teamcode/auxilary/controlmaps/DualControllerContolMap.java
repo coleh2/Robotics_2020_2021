136493013a18e0869f5e5e8684e790c9639ac16e
Duelephant <pmb221@students.needham.k12.ma.us>
2021-03-10 15:31:52 -0500

    trigger changed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auxilary/controlmaps/DualControllerContolMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auxilary/controlmaps/DualControllerContolMap.java
@@ -5,8 +5,8 @@ public class DualControllerContolMap extends ControlMap {
     public static String intake = "ternary(cross, -1, ternary(triangle, 1, ternary(leftTrigger, -1, 0)))";
     public static String drum = "ternary(dpadUp, -1, ternary(dpadDown, 1, ternary(gamepad2DpadDown, 1, ternary(leftTrigger, -1, ternary(gamepad2Cross, -1, ternary(gamepad2DpadUp, -1, 0))))))";
     public static String shooterArm = "ternary(greaterThan(gamepad2RightTrigger, 0.1), 0, 0.63)";
-    public static String flywheelRight = "scale(ternary(gamepad2Cross, -0.3, ternary(gamepad2LeftTrigger, 1, 0)), -1)";
-    public static String flywheelLeft  = "scale(ternary(gamepad2Cross, -0.3, ternary(gamepad2LeftTrigger, 1, 0)), 1)";
+    public static String flywheelRight = "scale(ternary(gamepad2Cross, -0.3, ternary(greaterThan(gamepad2LeftTrigger, 0.3), 1, 0)), -1)";
+    public static String flywheelLeft  = "scale(ternary(gamepad2Cross, -0.3, ternary(greaterThan(gamepad2LeftTrigger, 0.3), 1, 0)), 1)";
     public static String wobbleGrabRight = "ternary(gamepad2RightBumper, 1, 0)";
     public static String wobbleGrabLeft = "ternary(gamepad2RightBumper, 1, 0)";
     public static String wobbleArmRight = "ternary(gamepad2LeftBumper, 0.055, 0.086)";
