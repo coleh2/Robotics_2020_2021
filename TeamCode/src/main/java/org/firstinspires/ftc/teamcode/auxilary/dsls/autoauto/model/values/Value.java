01049762284f3c2a34dad3c756e0667fe212ad08
coleh2 <coleh@coleh.net>
2021-03-22 16:06:30 -0400

    Integer literals in autoauto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auxilary/dsls/autoauto/model/values/Value.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auxilary/dsls/autoauto/model/values/Value.java
@@ -12,7 +12,7 @@ public class Value {
 
     public static Value createProperValueType(String src) {
         String trimmed = src.trim();
-        if(trimmed.matches("-?[0-9]*\\.?[0-9]+?$")) return new NumericValue(trimmed);
+        if(trimmed.matches("^-?[0-9]*\\.?[0-9]+$")) return new NumericValue(trimmed);
         else if(trimmed.matches("^-?[0-9]*\\.?[0-9]+[a-z]+$")) return new UnitValue(trimmed);
         else if(trimmed.startsWith("[") && trimmed.endsWith("]")) return new ArrayLiteral(trimmed);
         else if(trimmed.indexOf('(') > -1) return new FunctionCall(trimmed);
