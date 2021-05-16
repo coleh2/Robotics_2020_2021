3119cd8dd9cf787b1346a80574d6adf1d6758059
Duelephant <pmb221@students.needham.k12.ma.us>
2021-05-16 12:39:38 -0400

    some stuff changed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auxilary/controlmaps/DualControllerContolMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auxilary/controlmaps/DualControllerContolMap.java
@@ -4,9 +4,10 @@ public class DualControllerContolMap extends ControlMap {
     public static String drive = "ternary( or(or(leftStickY, leftStickX), rightStickX), scale(vector3(deadzone(leftStickY, 0.1), deadzone(leftStickX, 0.05), rightStickX), ternary(rightStickX, 1.0, 1.3)), vector3(scale(deadzone(gamepad2LeftStickY, 0.15), 0.4), scale(deadzone(gamepad2LeftStickX, 0.15), 0.4), scale(gamepad2RightStickX, 0.4)))";
     public static String intake = "ternary(cross, -1, ternary(triangle, 1, ternary(leftTrigger, -1, 0)))";
     public static String drum = "ternary(dpadUp, -1, ternary(dpadDown, 1, ternary(gamepad2DpadDown, 1, ternary(leftTrigger, -1, ternary(gamepad2DpadUp, -1, 0)))))";
-    public static String spinner = "ternary(dpadRight, 0.2, ternary(gamepad2DpadRight, 0.2, ternary(gamepad2Cross, -0.1, ternary(dpadLeft, -0.1, ternary(leftTrigger, 0.1, 0)))))";
+    public static String spinner = "ternary(dpadRight, 0.5, ternary(gamepad2DpadRight, 0.5, ternary(gamepad2Cross, -0.5, ternary(dpadLeft, -0.5, ternary(leftTrigger, 0.5, 0)))))";
     public static String shooterArm = "ternary(greaterThan(gamepad2RightTrigger, 0.1), 0.3, 1)";
-    public static String flywheel = "scale(ternary(gampepad2Triangle, -0.3, ternary(greaterThan(gamepad2LeftTrigger, 0.3), 1, 0)), 1)";;
+//    public static String flywheel = "ternary(gampepad2Triangle, -0.3, ternary(greaterThan(gamepad2LeftTrigger, 0.3), 1, 0))";;
+    public static String flywheel = "ternary(gamepad2LeftTrigger, 1, 0)";
     public static String wobbleGrabRight = "ternary(gamepad2RightBumper, 1, 0)";
     public static String wobbleGrabLeft = "ternary(gamepad2RightBumper, 1, 0)";
     public static String wobbleArmRight = "ternary(gamepad2LeftBumper, 1, 0.2164)";
