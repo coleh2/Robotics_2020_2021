1a7f7a834c8e4b67de81eedb666577929c843e57
coleh2 <coleh@coleh.net>
2021-06-12 21:10:15 -0400

    feat: re-remove controls sources to prevent excessive compilation & delay

--- ControlsLanguagePlugin/src/main/java/net/coleh/controlslanguageplugin/ControlsIcons.java
+++
@@ -1,9 +0,0 @@
-package net.coleh.controlslanguageplugin;
-
-import com.intellij.openapi.util.IconLoader;
-
-import javax.swing.Icon;
-
-public class ControlsIcons {
-    public static final Icon FILE = IconLoader.getIcon("/icons/controls-16.png");
-}
